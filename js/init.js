a7e445e850a568ca521d4e113a6945a7ab507a55
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2016-04-29 12:04:01 -0400

    File Stuff

--- js/init.js
+++ js/init.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
-  var project_id = location.hash && parseInt(location.hash.substr(1)) || 10000160;
+  var project_id = 10000160; // default
+  if (location.hash) {
+     if (location.hash === '#file') {
+       if (sessionStorage.getItem('filename')) {
+         // signal to Scratch to load the file contents from localStorage
+         project_id = 'file';
+         } else {
+            // no file stored, reset to default
+            location.hash = '';
+        }
+        } else if (!Number.isNaN(Number(location.hash.substr(1)))) {
+            project_id = parseInt(location.hash.substr(1), 10);
+       }
+     }
   var scratch = new Scratch(project_id);
 });
